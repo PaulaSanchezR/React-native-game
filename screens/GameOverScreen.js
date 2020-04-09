3183e01b20014022b176e08635ac14e19191ea7f
PaulaSanchezR <paulasanchez.restrepo@gmail.com>
2020-04-09 17:16:55 -0400

    icons

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet, Button, Image } from 'react-native';
 import BodyText from '../components/BodyText';
 import TitleText from '../components/TitleText';
 import Colors from '../constants/colors';
-
+import MainBotton from '../components/MainButton';
 
 const GameOverScreens = props =>{
     return (
@@ -22,7 +22,7 @@ const GameOverScreens = props =>{
                 <BodyText style={styles.resultText}>Your phone needed ..<Text style={styles.highlight}>{props.roundsNumber}</Text>.. rounds to guess the number..<Text style={styles.highlight}>{props.userNumber}</Text>..</BodyText>
            </View>
             {/* <BodyText>Number was: </BodyText> */}
-            <Button title="New Game" onPress={props.onRestart} />
+            <MainBotton onPress={props.onRestart}>NEW GAME</MainBotton>
         </View>
     )
 
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -15,7 +15,7 @@ import Card from '../components/Card';
 import Input from '../components/Input';
 import NumberContainer from '../components/NumberContainer';
 import Colors from '../constants/colors';
-
+import MainBotton from '../components/MainButton';
 
 const StartGameScreen = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -54,7 +54,7 @@ const StartGameScreen = props => {
       <Card style={styles.summaryContainer}>
         <BodyText>You selected</BodyText>
         <NumberContainer>{selectedNumber}</NumberContainer>
-        <Button title="START GAME" onPress={() => props.onStartGame(selectedNumber)} />
+        <MainBotton onPress={() => props.onStartGame(selectedNumber)}>START GAME</MainBotton>
       </Card>
     );
   }
